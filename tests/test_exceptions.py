6eebe520cff92ab8d47503b4dfe8865f106029f4
leugimkm <leugim911@hotmail.com>
2022-01-27 22:06:49 -0500

    test file

---
+++ tests/test_exceptions.py
@@ -0,0 +1,12 @@
+import io
+import unittest
+from unittest.mock import patch, Mock
+
+
+class TestCodeSeekerException(unittest.TestCase):
+
+    ...
+
+
+if __name__ == '__main__':
+    unittest.main()
