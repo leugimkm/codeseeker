9946ebcded17d06c00627f0ca40aaddfb75c448b
leugimkm <leugim911@hotmail.com>
2022-01-31 23:22:31 -0500

    update and minor changes

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -2,7 +2,8 @@ import io
 import unittest
 from unittest.mock import patch, Mock
 
-from codeseeker.codeseeker import validate_response
+from codeseeker.validators import validate_response, validate_data_links
+
 
 class TestCodeSeekerException(unittest.TestCase):
 
@@ -12,12 +13,21 @@ class TestCodeSeekerException(unittest.TestCase):
             function()
         self.assertEqual(mock_stdout.getvalue(), expected)
     
-    @patch("codeseeker.codeseeker.requests.get")
+    @patch("codeseeker.seeker.requests.get")
     def test_validate_response(self, mock):
         mock = Mock(status_code=404)
         with self.assertRaises(Exception) as msg:
             validate_response(mock)
         self.assertEqual("Error: 404", str(msg.exception))
+
+    def test_validate_data_links(self):
+        data = []
+        with self.assertRaises(Exception) as msg:
+            validate_data_links(data)
+        self.assertEqual(
+            "Can't open in a web browser (there are no results).",
+            str(msg.exception)
+        )
 
 
 if __name__ == '__main__':
