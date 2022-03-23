f7677c1db272f63aba74c9406408b189e569a2e7
leugimkm <leugim911@hotmail.com>
2022-03-23 12:12:16 -0500

    version up

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="codeseeker",
-    version="0.0.8",
+    version="0.0.9",
     url="https://github.com/leugimkm/codeseeker",
     license="MIT",
     author="leugimkm",
